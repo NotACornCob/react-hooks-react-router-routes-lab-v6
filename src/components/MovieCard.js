37b38635aa6097f51b495fbba9526e1fe1179c13
NotACornCob <alex.kleinman@gmail.com>
2024-06-15 14:12:16 -0400

    final final

--- src/components/MovieCard.js
+++ src/components/MovieCard.js
@@ -6,12 +6,8 @@ function MovieCard({title, time, genres, id}) {
   
   return (
     <>
-      <header>
-      </header>
-      <body>
       <div><h2><Link to={`/movie/${id}`}>{title}</Link></h2></div>
       <div><p><Link to={`/movie/${id}`}>View Info</Link></p></div>
-    </body>
     </>
   );
 };
